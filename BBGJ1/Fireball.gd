662cd04c6503caa36341205af70edf2cd51ee375
Jeff <jeff.r.lane@gmail.com>
2020-11-22 13:26:27 -0500

    add new files

---
+++ BBGJ1/Fireball.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var speed = 15
+var direction = Vector2(1, 1).normalized()
+var creator
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += speed * direction
+
+
+func _on_Area2D_body_entered(body):
+	if not body == creator:
+		call_deferred("free")
